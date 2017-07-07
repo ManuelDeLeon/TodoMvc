112619c2364634b0aa8364a6b02893d761215a1b
ManuelDeLeon <manueldeleon@gmail.com>
2017-07-06 22:02:38 -0600

    Finish e2e tests

--- tests/App.e2e.js
+++ tests/App.e2e.js
@@ -8,46 +8,85 @@ describe('angularjs homepage', function () {
   });
 
   it('can add item', function () {
-    const input = mainPage.newItem;
-    expect(input.getText()).toEqual("");
-    input.sendKeys("AAA", protractor.Key.ENTER);
-    const itemsLeft = mainPage.itemsLeft;
-    expect(itemsLeft.getText()).toEqual("1 item left");
-    const selectedFilter = mainPage.selectedFilter;
-    expect(selectedFilter.getText()).toEqual('All');
+    expect(mainPage.newItem.getText()).toEqual("");
+    mainPage.newItem.sendKeys("AAA", protractor.Key.ENTER);
+    expect(mainPage.itemsLeft.getText()).toEqual("1 item left");
+    expect(mainPage.selectedFilter.getText()).toEqual('All');
     expect(mainPage.items.count()).toBe(1);
     const item = mainPage.items.get(0);
     expect(item.checkbox.getAttribute('checked')).toBeFalsy();
     expect(item.label.getText()).toEqual('AAA');
     expect(item.button.isDisplayed()).toBe(false);
-    expect(input.getText()).toEqual("");
-    expect(input.getId()).toEqual(browser.driver.switchTo().activeElement().getId());
+    expect(mainPage.newItem.getText()).toEqual("");
+    expect(mainPage.newItem.getId()).toEqual(browser.driver.switchTo().activeElement().getId());
   });
 
   it('can delete item', ()=>{
-    const input = mainPage.newItem;
-    input.sendKeys("AAA", protractor.Key.ENTER);
-    const main = by.css('section.main');
-    expect(element(main).isDisplayed()).toBe(true);
-    const footer = by.css('footer.footer');
-    expect(element(footer).isDisplayed()).toBe(true);
-    browser.actions().mouseMove(element(by.css('ul.todo-list li'))).perform();
-    const button = element(by.css('button.destroy'));
-    expect(button.isDisplayed()).toBe(true);
-    browser.actions().click(button).perform();
-    expect(element(main).isPresent()).toBe(false);
-    expect(element(footer).isPresent()).toBe(false);
-    expect(input.getText()).toEqual("");
+    mainPage.newItem.sendKeys("AAA", protractor.Key.ENTER);
+    expect(mainPage.mainBlock.isDisplayed()).toBe(true);
+    expect(mainPage.footerBlock.isDisplayed()).toBe(true);
+    const item = mainPage.items.get(0);
+    browser.actions().mouseMove(item.row).perform();
+    expect(item.button.isDisplayed()).toBe(true);
+    item.button.click();
+    expect(mainPage.mainBlock.isPresent()).toBe(false);
+    expect(mainPage.footerBlock.isPresent()).toBe(false);
+    expect(mainPage.newItem.getText()).toEqual("");
   })
 
   it(`can select All/Active/Completed`, ()=>{
-    const input = mainPage.newItem;
-    input.sendKeys("AAA", protractor.Key.ENTER);
-    const aaaCheckbox = element(by.css('.view input[type="checkbox"]:not(:checked)'));
-    input.sendKeys("BBB", protractor.Key.ENTER);
-    expect(element(by.css('span.todo-count')).getText()).toEqual("2 items left");
-    browser.actions().click(aaaCheckbox).perform();
-    const bbbCheckbox = element(by.css('.view input[type="checkbox"]:not(:checked)'));
-    expect(element(by.css('span.todo-count')).getText()).toEqual("1 item left");
+    mainPage.newItem.sendKeys("AAA", protractor.Key.ENTER);
+    expect(mainPage.filterAll.getAttribute('class')).toContain("selected");
+    mainPage.newItem.sendKeys("BBB", protractor.Key.ENTER);
+    expect(mainPage.clearCompleted.isPresent()).toBe(false);
+    expect(mainPage.itemsLeft.getText()).toEqual("2 items left");
+    expect(mainPage.items.count()).toBe(2);
+    mainPage.items.get(0).checkbox.click();
+    expect(mainPage.clearCompleted.isPresent()).toBe(true);
+    expect(mainPage.itemsLeft.getText()).toEqual("1 item left");
+    mainPage.filterActive.click();
+    expect(mainPage.items.count()).toBe(1);
+    expect(mainPage.items.get(0).label.getText()).toBe("BBB");
+    mainPage.filterCompleted.click();
+    expect(mainPage.items.count()).toBe(1);
+    expect(mainPage.items.get(0).label.getText()).toBe("AAA");
+    mainPage.filterAll.click();
+    expect(mainPage.items.count()).toBe(2);
+    expect(mainPage.items.get(0).label.getText()).toBe("AAA");
+    expect(mainPage.items.get(1).label.getText()).toBe("BBB");
+  })
+
+  it(`clears completed`, () => {
+    mainPage.newItem.sendKeys("AAA", protractor.Key.ENTER);
+    mainPage.newItem.sendKeys("BBB", protractor.Key.ENTER);
+    mainPage.newItem.sendKeys("CCC", protractor.Key.ENTER);
+    mainPage.items.get(0).checkbox.click();
+    mainPage.items.get(2).checkbox.click();
+    mainPage.clearCompleted.click();
+    expect(mainPage.items.count()).toBe(1);
+    expect(mainPage.items.get(0).label.getText()).toBe("BBB");
+  })
+
+  it(`toggles completed`, () => {
+    mainPage.newItem.sendKeys("AAA", protractor.Key.ENTER);
+    mainPage.newItem.sendKeys("BBB", protractor.Key.ENTER);
+    mainPage.newItem.sendKeys("CCC", protractor.Key.ENTER);
+    mainPage.items.get(0).checkbox.click();
+    mainPage.items.get(2).checkbox.click();
+    mainPage.toggleCompleted.click();
+    expect(mainPage.items.count()).toBe(3);
+    expect(mainPage.items.get(0).checkbox.getAttribute('checked')).toBeTruthy();
+    expect(mainPage.items.get(1).checkbox.getAttribute('checked')).toBeTruthy();
+    expect(mainPage.items.get(2).checkbox.getAttribute('checked')).toBeTruthy();
+    mainPage.toggleCompleted.click();
+    expect(mainPage.items.count()).toBe(3);
+    expect(mainPage.items.get(0).checkbox.getAttribute('checked')).toBeFalsy();
+    expect(mainPage.items.get(1).checkbox.getAttribute('checked')).toBeFalsy();
+    expect(mainPage.items.get(2).checkbox.getAttribute('checked')).toBeFalsy();
+    mainPage.toggleCompleted.click();
+    expect(mainPage.items.count()).toBe(3);
+    expect(mainPage.items.get(0).checkbox.getAttribute('checked')).toBeTruthy();
+    expect(mainPage.items.get(1).checkbox.getAttribute('checked')).toBeTruthy();
+    expect(mainPage.items.get(2).checkbox.getAttribute('checked')).toBeTruthy();
   })
 });
--- tests/pages/Main.js
+++ tests/pages/Main.js
@@ -1,8 +1,17 @@
 export default () => {
   const mainPage = {
     newItem: element(by.css('header.header input[type=text]')),
+    mainBlock: element(by.css('section.main')),
+    headerBlock: element(by.css('header.header')),
+    footerBlock: element(by.css('footer.footer')),
+    toggleCompleted: element(by.css('section.main label')),
+    clearCompleted: element(by.css('footer.footer button')),
+    filterAll: element.all(by.css('footer.footer ul.filters a')).get(0),
+    filterActive: element.all(by.css('footer.footer ul.filters a')).get(1),
+    filterCompleted: element.all(by.css('footer.footer ul.filters a')).get(2),
     itemsLeft: element(by.css('span.todo-count')),
     selectedFilter: element(by.css('.filters a.selected')),
+    itemsRows: element.all(by.css('ul.todo-list li')),
     itemsCheckboxes: element.all(by.css('ul.todo-list li .view input[type=checkbox]')),
     itemsLabels: element.all(by.css('ul.todo-list li .view label')),
     itemsButtons: element.all(by.css('ul.todo-list li .view button.destroy')),
@@ -11,10 +20,11 @@ export default () => {
         return element.all(by.css('ul.todo-list li')).count();
       },
       get(index) {
+        const row = mainPage.itemsRows.get(index);
         const checkbox = mainPage.itemsCheckboxes.get(index);
         const label = mainPage.itemsLabels.get(index);
         const button = mainPage.itemsButtons.get(index);
-        return { checkbox, label, button };
+        return { row, checkbox, label, button };
       }
     }
   }
